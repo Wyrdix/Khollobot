5f82e87cdf3e203f4a2a6749d2ec308dc77d2279
Wyrdix <nathancoquerel@gmail.com>
2023-07-10 17:03:37 +0200

    Add plugins command which send a list of all plugins with status and details

--- src/main/java/com/wyrdix/khollobot/plugin/DefaultPlugin.java
+++ src/main/java/com/wyrdix/khollobot/plugin/DefaultPlugin.java
@@ -1,10 +1,13 @@
 package com.wyrdix.khollobot.plugin;
 
+import com.wyrdix.khollobot.command.PluginsCommand;
+
 @PluginInfo(id = "default", name = "Défaut", version = "1.0-SNAPSHOT", author = "Wyrdix")
 public class DefaultPlugin implements Plugin {
 
     @Override
     public void onEnable() {
+        getJda().upsertCommand(PluginsCommand.getInstance().getData()).queue();
     }
 
     @Override
