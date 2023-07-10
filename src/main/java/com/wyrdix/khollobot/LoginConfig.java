650ca4ff58e01554de696c7cbdf61f6197a87963
Wyrdix <nathancoquerel@gmail.com>
2023-07-10 13:57:32 +0200

    Now user resource getter instead of file one

--- src/main/java/com/wyrdix/khollobot/LoginConfig.java
+++ src/main/java/com/wyrdix/khollobot/LoginConfig.java
@@ -4,10 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class LoginConfig {
 
@@ -24,22 +21,22 @@ public class LoginConfig {
 
     public static LoginConfig getLogin(){
         if(INSTANCE == null){
-            System.out.println("Loading login file located at : "+new File("login.json").getAbsolutePath());
-            FileReader reader;
-            try {
-                reader = new FileReader("login.json");
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+
+            InputStream inputStream = LoginConfig.class.getClassLoader().getResourceAsStream("login.json");
+            assert inputStream != null;
+            InputStreamReader streamReader = new InputStreamReader(inputStream);
+
+            System.out.println("Loading login resource file, may be located at : "+new File("login.json").getAbsolutePath());
 
             try {
-                INSTANCE = new Gson().fromJson(reader, LoginConfig.class);
+                INSTANCE = new Gson().fromJson(streamReader, LoginConfig.class);
             } catch (JsonSyntaxException | JsonIOException e) {
                 throw new RuntimeException(e);
             }
 
             try {
-                reader.close();
+                streamReader.close();
+                inputStream.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
