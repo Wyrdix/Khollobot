e26dbd96e5f473f3258070fb2e0cddbfa2a474e1
Wyrdix <nathancoquerel@gmail.com>
2023-07-10 15:01:21 +0200

    Add default plugin

--- src/main/java/com/wyrdix/khollobot/KholloBot.java
+++ src/main/java/com/wyrdix/khollobot/KholloBot.java
@@ -1,12 +1,54 @@
 package com.wyrdix.khollobot;
 
 
-import net.dv8tion.jda.api.JDA;
+import com.wyrdix.khollobot.command.KCommand;
+import com.wyrdix.khollobot.plugin.DefaultPlugin;
+import com.wyrdix.khollobot.plugin.Plugin;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class KholloBot {
 
+    private static final Map<Class<? extends Plugin>, Plugin> PLUGIN_MAP = new HashMap<>();
+    private static net.dv8tion.jda.api.JDA JDA = null;
+
     public static void main(String[] args) {
-        JDA api = JDABuilder.createDefault(LoginConfig.getLogin().getDiscordToken()).build();
+        setJDA(JDABuilder.createDefault(LoginConfig.getLogin().getDiscordToken()).build());
+
+        addPlugin(new DefaultPlugin());
+
+        getJDA().addEventListener(new ListenerAdapter() {
+            @Override
+            public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+                String commandId = event.getInteraction().getCommandString().substring(1);
+
+                KCommand command = KCommand.COMMAND_MAP.get(commandId);
+                if(command != null) command.execute(event);
+            }
+        });
+
+        PLUGIN_MAP.values().forEach(Plugin::onEnable);
+    }
+
+    private static void addPlugin(Plugin plugin){
+        PLUGIN_MAP.put(plugin.getClass(), plugin);
+    }
+
+    public static net.dv8tion.jda.api.JDA getJDA() {
+        return JDA;
+    }
+
+    private static void setJDA(net.dv8tion.jda.api.JDA JDA) {
+        KholloBot.JDA = JDA;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static<T extends Plugin> T getPlugin(Class<T> pluginClazz) {
+        return (T) PLUGIN_MAP.get(pluginClazz);
     }
 }
---
+++ src/main/java/com/wyrdix/khollobot/plugin/DefaultPlugin.java
@@ -0,0 +1,14 @@
+package com.wyrdix.khollobot.plugin;
+
+@PluginInfo(id = "default", name = "Défaut", version = "1.0-SNAPSHOT", author = "Wyrdix")
+public class DefaultPlugin implements Plugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void save() {
+
+    }
+}
