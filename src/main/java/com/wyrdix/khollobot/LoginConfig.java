52eacff132752b230091021d55b37a6ce50ecc5e
Wyrdix <nathancoquerel@gmail.com>
2023-07-17 19:41:33 +0200

    Add Mail redirector

--- src/main/java/com/wyrdix/khollobot/LoginConfig.java
+++ src/main/java/com/wyrdix/khollobot/LoginConfig.java
@@ -4,29 +4,30 @@ import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class LoginConfig {
 
     private static LoginConfig INSTANCE = null;
-
-    public String getDiscordToken() {
-        return discord_token;
-    }
-
     public String discord_token;
+    public String mail_password;
+    public String mail_username;
+
     public LoginConfig() {
 
     }
 
-    public static LoginConfig getLogin(){
-        if(INSTANCE == null){
+    public static LoginConfig getLogin() {
+        if (INSTANCE == null) {
 
             InputStream inputStream = LoginConfig.class.getClassLoader().getResourceAsStream("login.json");
             assert inputStream != null;
             InputStreamReader streamReader = new InputStreamReader(inputStream);
 
-            System.out.println("Loading login resource file, may be located at : "+new File("login.json").getAbsolutePath());
+            System.out.println("Loading login resource file, may be located at : " + new File("login.json").getAbsolutePath());
 
             try {
                 INSTANCE = new Gson().fromJson(streamReader, LoginConfig.class);
@@ -43,5 +44,17 @@ public class LoginConfig {
         }
 
         return INSTANCE;
+    }
+
+    public String getDiscordToken() {
+        return discord_token;
+    }
+
+    public String getMail_password() {
+        return mail_password;
+    }
+
+    public String getMailUsername() {
+        return mail_username;
     }
 }
